7be0561db9b92ddba9d4fabb4dfadfb09d7e7d11
vipul2001 <47273021+vipul2001@users.noreply.github.com>
2019-12-15 22:54:44 +0530

    Added Code Files

---
+++ Model_Evaluation _and_Hyper_Parameter_Tuning/code.py
@@ -0,0 +1,333 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+# In[2]:
+
+
+df = pd.read_csv('https://archive.ics.uci.edu/ml/'
+                 'machine-learning-databases'
+                 '/breast-cancer-wisconsin/wdbc.data', header=None)
+
+
+# In[3]:
+
+
+df.head()
+
+
+# In[4]:
+
+
+
+from sklearn.preprocessing import LabelEncoder
+
+X = df.loc[:, 2:].values
+y = df.loc[:, 1].values
+le = LabelEncoder()
+y = le.fit_transform(y)
+le.classes_
+
+
+# In[5]:
+
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test =     train_test_split(X, y, 
+                     test_size=0.20,
+                     stratify=y,
+                     random_state=1)
+
+
+# # Making Pipelines using sklearn.pipeline
+
+# In[7]:
+
+
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LogisticRegression
+from sklearn.pipeline import make_pipeline
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        PCA(n_components=2),
+                        LogisticRegression(random_state=1, solver='lbfgs'))
+
+pipe_lr.fit(X_train, y_train)
+y_pred = pipe_lr.predict(X_test)
+print('Test Accuracy: %.3f' % pipe_lr.score(X_test, y_test))
+
+
+# # Using K-fold CrossValidation on Data
+
+# In[9]:
+
+
+import numpy as np
+from sklearn.model_selection import StratifiedKFold
+    
+
+kfold = StratifiedKFold(n_splits=10).split(X_train, y_train)
+
+scores = []
+for k, (train, test) in enumerate(kfold):
+    pipe_lr.fit(X_train[train], y_train[train])
+    score = pipe_lr.score(X_train[test], y_train[test])
+    scores.append(score)
+    print('Fold: %2d, Class dist.: %s, Acc: %.3f' % (k+1,
+          np.bincount(y_train[train]), score))
+    
+print('\nCV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+
+# In[11]:
+
+
+from sklearn.model_selection import cross_val_score
+
+scores = cross_val_score(estimator=pipe_lr,
+                         X=X_train,
+                         y=y_train,
+                         cv=10,
+                         n_jobs=1)
+print('CV accuracy scores: %s' % scores)
+print('CV accuracy: %.3f +/- %.3f' % (np.mean(scores), np.std(scores)))
+
+
+# # Analysing Algorithm based On learning_curve
+
+# In[18]:
+
+
+import matplotlib.pyplot as plt
+from sklearn.model_selection import learning_curve
+
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        LogisticRegression(penalty='l2', random_state=1,
+                                           solver='lbfgs', max_iter=10000))
+
+train_sizes, train_scores, test_scores =                learning_curve(estimator=pipe_lr,
+                               X=X_train,
+                               y=y_train,
+                               train_sizes=np.linspace(0.1, 1.0, 10),
+                               cv=10,
+                               n_jobs=1)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(train_sizes, train_mean,
+         color='blue', marker='o',
+         markersize=5, label='Training accuracy')
+
+plt.fill_between(train_sizes,
+                 train_mean + train_std,
+                 train_mean - train_std,
+                 alpha=0.5, color='blue')
+
+plt.plot(train_sizes, test_mean,
+         color='green', linestyle='--',
+         marker='s', markersize=5,
+         label='Validation accuracy')
+
+plt.fill_between(train_sizes,
+                 test_mean + test_std,
+                 test_mean - test_std,
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xlabel('Number of training examples')
+plt.ylabel('Accuracy')
+plt.legend(loc='lower right')
+plt.ylim([0.8, 1.03])
+plt.tight_layout()
+# plt.savefig('images/06_05.png', dpi=300)
+plt.show()
+
+
+# # Model Accuracy using validation_curve
+
+# In[20]:
+
+
+
+from sklearn.model_selection import validation_curve
+
+
+param_range = [0.001, 0.01, 0.1, 1.0, 10.0, 100.0]
+train_scores, test_scores = validation_curve(
+                estimator=pipe_lr, 
+                X=X_train, 
+                y=y_train, 
+                param_name='logisticregression__C', 
+                param_range=param_range,
+                cv=10)
+
+train_mean = np.mean(train_scores, axis=1)
+train_std = np.std(train_scores, axis=1)
+test_mean = np.mean(test_scores, axis=1)
+test_std = np.std(test_scores, axis=1)
+
+plt.plot(param_range, train_mean, 
+         color='blue', marker='o', 
+         markersize=5, label='Training accuracy')
+
+plt.fill_between(param_range, train_mean + train_std,
+                 train_mean - train_std, alpha=0.15,
+                 color='blue')
+
+plt.plot(param_range, test_mean, 
+         color='green', linestyle='--', 
+         marker='s', markersize=5, 
+         label='Validation accuracy')
+
+plt.fill_between(param_range, 
+                 test_mean + test_std,
+                 test_mean - test_std, 
+                 alpha=0.15, color='green')
+
+plt.grid()
+plt.xscale('log')
+plt.legend(loc='lower right')
+plt.xlabel('Parameter C')
+plt.ylabel('Accuracy')
+plt.ylim([0.8, 1.0])
+plt.tight_layout()
+plt.show()
+
+
+# # Hyper Parameter Tuning via GridSearchCV
+
+# In[21]:
+
+
+
+from sklearn.model_selection import GridSearchCV
+from sklearn.svm import SVC
+
+pipe_svc = make_pipeline(StandardScaler(),
+                         SVC(random_state=1))
+
+param_range = [0.0001, 0.001, 0.01, 0.1, 1.0, 10.0, 100.0, 1000.0]
+
+param_grid = [{'svc__C': param_range, 
+               'svc__kernel': ['linear']},
+              {'svc__C': param_range, 
+               'svc__gamma': param_range, 
+               'svc__kernel': ['rbf']}]
+
+gs = GridSearchCV(estimator=pipe_svc, 
+                  param_grid=param_grid, 
+                  scoring='accuracy', 
+                  refit=True,
+                  cv=10,
+                  n_jobs=-1)
+gs = gs.fit(X_train, y_train)
+print(gs.best_score_)
+print(gs.best_params_)
+
+
+# # Plotting and Using Confusion Matrix
+
+# In[24]:
+
+
+from sklearn.metrics import confusion_matrix
+
+pipe_svc.fit(X_train, y_train)
+y_pred = pipe_svc.predict(X_test)
+confmat = confusion_matrix(y_true=y_test, y_pred=y_pred)
+print(confmat)
+fig, ax = plt.subplots(figsize=(5,5))
+ax.matshow(confmat, cmap=plt.cm.Blues, alpha=0.3)
+for i in range(confmat.shape[0]):
+    for j in range(confmat.shape[1]):
+        ax.text(x=j, y=i, s=confmat[i, j], va='center', ha='center')
+
+plt.xlabel('Predicted label')
+plt.ylabel('True label')
+
+plt.tight_layout()
+#plt.savefig('images/06_09.png', dpi=300)
+plt.show()
+
+
+# # Plotting ROC Curve of Classification
+
+# In[25]:
+
+
+from sklearn.metrics import roc_curve, auc
+from scipy import interp
+
+pipe_lr = make_pipeline(StandardScaler(),
+                        PCA(n_components=2),
+                        LogisticRegression(penalty='l2', 
+                                           random_state=1,
+                                           solver='lbfgs',
+                                           C=100.0))
+
+X_train2 = X_train[:, [4, 14]]
+    
+
+cv = list(StratifiedKFold(n_splits=3).split(X_train, y_train))
+
+fig = plt.figure(figsize=(7, 5))
+
+mean_tpr = 0.0
+mean_fpr = np.linspace(0, 1, 100)
+all_tpr = []
+
+for i, (train, test) in enumerate(cv):
+    probas = pipe_lr.fit(X_train2[train],
+                         y_train[train]).predict_proba(X_train2[test])
+
+    fpr, tpr, thresholds = roc_curve(y_train[test],
+                                     probas[:, 1],
+                                     pos_label=1)
+    mean_tpr += interp(mean_fpr, fpr, tpr)
+    mean_tpr[0] = 0.0
+    roc_auc = auc(fpr, tpr)
+    plt.plot(fpr,
+             tpr,
+             label='ROC fold %d (area = %0.2f)'
+                   % (i+1, roc_auc))
+
+plt.plot([0, 1],
+         [0, 1],
+         linestyle='--',
+         color=(0.6, 0.6, 0.6),
+         label='Random guessing')
+
+mean_tpr /= len(cv)
+mean_tpr[-1] = 1.0
+mean_auc = auc(mean_fpr, mean_tpr)
+plt.plot(mean_fpr, mean_tpr, 'k--',
+         label='Mean ROC (area = %0.2f)' % mean_auc, lw=2)
+plt.plot([0, 0, 1],
+         [0, 1, 1],
+         linestyle=':',
+         color='black',
+         label='Perfect performance')
+
+plt.xlim([-0.05, 1.05])
+plt.ylim([-0.05, 1.05])
+plt.xlabel('False positive rate')
+plt.ylabel('True positive rate')
+plt.legend(loc="lower right")
+
+plt.tight_layout()
+# plt.savefig('images/06_10.png', dpi=300)
+plt.show()
+
