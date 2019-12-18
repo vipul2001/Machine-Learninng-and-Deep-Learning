b88611ea0905142d7eb4b8b91a9d94b20ef3dbd1
vipul2001 <47273021+vipul2001@users.noreply.github.com>
2019-12-18 18:28:16 +0530

    Add files via upload

---
+++ Regression_Analysis/code.py
@@ -0,0 +1,535 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[37]:
+
+
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/rasbt/'
+                 'python-machine-learning-book-3rd-edition/'
+                 'master/ch10/housing.data.txt',
+                 header=None,
+                 sep='\s+')
+
+df.columns = ['CRIM', 'ZN', 'INDUS', 'CHAS', 
+              'NOX', 'RM', 'AGE', 'DIS', 'RAD', 
+              'TAX', 'PTRATIO', 'B', 'LSTAT', 'MEDV']
+df.head()
+
+
+# # Exploratory data Aanlysis
+
+# In[7]:
+
+
+import matplotlib.pyplot as plt
+from mlxtend.plotting import scatterplotmatrix
+
+
+# In[8]:
+
+
+cols = ['LSTAT', 'INDUS', 'NOX', 'RM', 'MEDV']
+
+scatterplotmatrix(df[cols].values, figsize=(10, 8), 
+                  names=cols, alpha=0.5)
+plt.tight_layout()
+#plt.savefig('images/10_03.png', dpi=300)
+plt.show()
+
+
+# In[9]:
+
+
+import numpy as np
+from mlxtend.plotting import heatmap
+
+
+cm = np.corrcoef(df[cols].values.T)
+hm = heatmap(cm, row_names=cols, column_names=cols)
+
+# plt.savefig('images/10_04.png', dpi=300)
+plt.show()
+
+
+# # Linear Regression from Scratch
+
+# In[2]:
+
+
+class LinearRegressionGD(object):
+
+    def __init__(self, eta=0.001, n_iter=20):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        self.w_ = np.zeros(1 + X.shape[1])
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+            output = self.net_input(X)
+            errors = (y - output)
+            self.w_[1:] += self.eta * X.T.dot(errors)
+            self.w_[0] += self.eta * errors.sum()
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+        return self
+
+    def net_input(self, X):
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        return self.net_input(X)
+
+
+# In[5]:
+
+
+import numpy as np
+X = df[['RM']].values
+y = df['MEDV'].values
+
+
+# In[6]:
+
+
+from sklearn.preprocessing import StandardScaler
+
+
+sc_x = StandardScaler()
+sc_y = StandardScaler()
+X_std = sc_x.fit_transform(X)
+y_std = sc_y.fit_transform(y[:, np.newaxis]).flatten()
+
+
+# In[11]:
+
+
+lr = LinearRegressionGD()
+lr.fit(X_std, y_std)
+
+
+# In[12]:
+
+
+plt.plot(range(1, lr.n_iter+1), lr.cost_)
+plt.ylabel('SSE')
+plt.xlabel('Epoch')
+#plt.tight_layout()
+#plt.savefig('images/10_05.png', dpi=300)
+plt.show()
+
+
+# In[13]:
+
+
+def lin_regplot(X, y, model):
+    plt.scatter(X, y, c='steelblue', edgecolor='white', s=70)
+    plt.plot(X, model.predict(X), color='black', lw=2)    
+    return
+
+
+# In[14]:
+
+
+lin_regplot(X_std, y_std, lr)
+plt.xlabel('Average number of rooms [RM] (standardized)')
+plt.ylabel('Price in $1000s [MEDV] (standardized)')
+
+#plt.savefig('images/10_06.png', dpi=300)
+plt.show()
+
+
+# In[15]:
+
+
+num_rooms_std = sc_x.transform(np.array([[5.0]]))
+price_std = lr.predict(num_rooms_std)
+print("Price in $1000s: %.3f" % sc_y.inverse_transform(price_std))
+
+
+# # RANSAC Regressor
+
+# In[17]:
+
+
+from sklearn.linear_model import RANSACRegressor
+from sklearn.linear_model import LinearRegression
+ransac = RANSACRegressor(LinearRegression(), 
+                         max_trials=100, 
+                         min_samples=50, 
+                         loss='absolute_loss', 
+                         residual_threshold=5.0, 
+                         random_state=0)
+
+
+ransac.fit(X, y)
+
+inlier_mask = ransac.inlier_mask_
+outlier_mask = np.logical_not(inlier_mask)
+
+line_X = np.arange(3, 10, 1)
+line_y_ransac = ransac.predict(line_X[:, np.newaxis])
+plt.scatter(X[inlier_mask], y[inlier_mask],
+            c='steelblue', edgecolor='white', 
+            marker='o', label='Inliers')
+plt.scatter(X[outlier_mask], y[outlier_mask],
+            c='limegreen', edgecolor='white', 
+            marker='s', label='Outliers')
+plt.plot(line_X, line_y_ransac, color='black', lw=2)   
+plt.xlabel('Average number of rooms [RM]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.legend(loc='upper left')
+
+#plt.savefig('images/10_08.png', dpi=300)
+plt.show()
+
+
+# In[18]:
+
+
+from sklearn.model_selection import train_test_split
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
+
+
+# In[19]:
+
+
+
+slr = LinearRegression()
+
+slr.fit(X_train, y_train)
+y_train_pred = slr.predict(X_train)
+y_test_pred = slr.predict(X_test)
+
+
+# In[20]:
+
+
+import numpy as np
+import scipy as sp
+
+ary = np.array(range(100000))
+
+
+# In[21]:
+
+
+plt.scatter(y_train_pred,  y_train_pred - y_train,
+            c='steelblue', marker='o', edgecolor='white',
+            label='Training data')
+plt.scatter(y_test_pred,  y_test_pred - y_test,
+            c='limegreen', marker='s', edgecolor='white',
+            label='Test data')
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, color='black', lw=2)
+plt.xlim([-10, 50])
+plt.tight_layout()
+
+# plt.savefig('images/10_09.png', dpi=300)
+plt.show()
+
+
+# In[22]:
+
+
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error
+
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+
+
+# # LASSO,RIDGE AND ElasticNet
+
+# In[23]:
+
+
+
+from sklearn.linear_model import Lasso
+
+lasso = Lasso(alpha=0.1)
+lasso.fit(X_train, y_train)
+y_train_pred = lasso.predict(X_train)
+y_test_pred = lasso.predict(X_test)
+print(lasso.coef_)
+
+
+# In[24]:
+
+
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+
+
+# In[25]:
+
+
+from sklearn.linear_model import Ridge
+ridge = Ridge(alpha=1.0)
+
+
+# In[26]:
+
+
+from sklearn.linear_model import Lasso
+lasso = Lasso(alpha=1.0)
+
+
+# In[27]:
+
+
+from sklearn.linear_model import ElasticNet
+elanet = ElasticNet(alpha=1.0, l1_ratio=0.5)
+
+
+# # Polynomial Regression
+
+# In[28]:
+
+
+X = np.array([258.0, 270.0, 294.0, 
+              320.0, 342.0, 368.0, 
+              396.0, 446.0, 480.0, 586.0])\
+             [:, np.newaxis]
+
+y = np.array([236.4, 234.4, 252.8, 
+              298.6, 314.2, 342.2, 
+              360.8, 368.0, 391.2,
+              390.8])
+
+
+# In[29]:
+
+
+
+from sklearn.preprocessing import PolynomialFeatures
+
+lr = LinearRegression()
+pr = LinearRegression()
+quadratic = PolynomialFeatures(degree=2)
+X_quad = quadratic.fit_transform(X)
+
+
+# In[30]:
+
+
+# fit linear features
+lr.fit(X, y)
+X_fit = np.arange(250, 600, 10)[:, np.newaxis]
+y_lin_fit = lr.predict(X_fit)
+
+# fit quadratic features
+pr.fit(X_quad, y)
+y_quad_fit = pr.predict(quadratic.fit_transform(X_fit))
+
+# plot results
+plt.scatter(X, y, label='Training points')
+plt.plot(X_fit, y_lin_fit, label='Linear fit', linestyle='--')
+plt.plot(X_fit, y_quad_fit, label='Quadratic fit')
+plt.xlabel('Explanatory variable')
+plt.ylabel('Predicted or known target values')
+plt.legend(loc='upper left')
+
+plt.tight_layout()
+#plt.savefig('images/10_11.png', dpi=300)
+plt.show()
+
+
+# In[31]:
+
+
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+
+regr = LinearRegression()
+
+# create quadratic features
+quadratic = PolynomialFeatures(degree=2)
+cubic = PolynomialFeatures(degree=3)
+X_quad = quadratic.fit_transform(X)
+X_cubic = cubic.fit_transform(X)
+
+# fit features
+X_fit = np.arange(X.min(), X.max(), 1)[:, np.newaxis]
+
+regr = regr.fit(X, y)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y, regr.predict(X))
+
+regr = regr.fit(X_quad, y)
+y_quad_fit = regr.predict(quadratic.fit_transform(X_fit))
+quadratic_r2 = r2_score(y, regr.predict(X_quad))
+
+regr = regr.fit(X_cubic, y)
+y_cubic_fit = regr.predict(cubic.fit_transform(X_fit))
+cubic_r2 = r2_score(y, regr.predict(X_cubic))
+
+
+# plot results
+plt.scatter(X, y, label='Training points', color='lightgray')
+
+plt.plot(X_fit, y_lin_fit, 
+         label='Linear (d=1), $R^2=%.2f$' % linear_r2, 
+         color='blue', 
+         lw=2, 
+         linestyle=':')
+
+plt.plot(X_fit, y_quad_fit, 
+         label='Quadratic (d=2), $R^2=%.2f$' % quadratic_r2,
+         color='red', 
+         lw=2,
+         linestyle='-')
+
+plt.plot(X_fit, y_cubic_fit, 
+         label='Cubic (d=3), $R^2=%.2f$' % cubic_r2,
+         color='green', 
+         lw=2, 
+         linestyle='--')
+
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000s [MEDV]')
+plt.legend(loc='upper right')
+
+#plt.savefig('images/10_12.png', dpi=300)
+plt.show()
+
+
+# In[32]:
+
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+
+# transform features
+X_log = np.log(X)
+y_sqrt = np.sqrt(y)
+
+# fit features
+X_fit = np.arange(X_log.min()-1, X_log.max()+1, 1)[:, np.newaxis]
+
+regr = regr.fit(X_log, y_sqrt)
+y_lin_fit = regr.predict(X_fit)
+linear_r2 = r2_score(y_sqrt, regr.predict(X_log))
+
+# plot results
+plt.scatter(X_log, y_sqrt, label='Training points', color='lightgray')
+
+plt.plot(X_fit, y_lin_fit, 
+         label='Linear (d=1), $R^2=%.2f$' % linear_r2, 
+         color='blue', 
+         lw=2)
+
+plt.xlabel('log(% lower status of the population [LSTAT])')
+plt.ylabel('$\sqrt{Price \; in \; \$1000s \; [MEDV]}$')
+plt.legend(loc='lower left')
+
+plt.tight_layout()
+#plt.savefig('images/10_13.png', dpi=300)
+plt.show()
+
+
+# # Decision Tree Regressor
+
+# In[33]:
+
+
+from sklearn.tree import DecisionTreeRegressor
+
+X = df[['LSTAT']].values
+y = df['MEDV'].values
+
+tree = DecisionTreeRegressor(max_depth=3)
+tree.fit(X, y)
+
+sort_idx = X.flatten().argsort()
+
+lin_regplot(X[sort_idx], y[sort_idx], tree)
+plt.xlabel('% lower status of the population [LSTAT]')
+plt.ylabel('Price in $1000s [MEDV]')
+#plt.savefig('images/10_14.png', dpi=300)
+plt.show()
+
+
+# In[34]:
+
+
+X = df.iloc[:, :-1].values
+y = df['MEDV'].values
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.4, random_state=1)
+
+
+# # Random Forest Regressor
+
+# In[35]:
+
+
+
+from sklearn.ensemble import RandomForestRegressor
+
+forest = RandomForestRegressor(n_estimators=1000, 
+                               criterion='mse', 
+                               random_state=1, 
+                               n_jobs=-1)
+forest.fit(X_train, y_train)
+y_train_pred = forest.predict(X_train)
+y_test_pred = forest.predict(X_test)
+
+print('MSE train: %.3f, test: %.3f' % (
+        mean_squared_error(y_train, y_train_pred),
+        mean_squared_error(y_test, y_test_pred)))
+print('R^2 train: %.3f, test: %.3f' % (
+        r2_score(y_train, y_train_pred),
+        r2_score(y_test, y_test_pred)))
+
+
+# In[36]:
+
+
+plt.scatter(y_train_pred,  
+            y_train_pred - y_train, 
+            c='steelblue',
+            edgecolor='white',
+            marker='o', 
+            s=35,
+            alpha=0.9,
+            label='Training data')
+plt.scatter(y_test_pred,  
+            y_test_pred - y_test, 
+            c='limegreen',
+            edgecolor='white',
+            marker='s', 
+            s=35,
+            alpha=0.9,
+            label='Test data')
+
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.legend(loc='upper left')
+plt.hlines(y=0, xmin=-10, xmax=50, lw=2, color='black')
+plt.xlim([-10, 50])
+plt.tight_layout()
+
+#plt.savefig('images/10_15.png', dpi=300)
+plt.show()
+
