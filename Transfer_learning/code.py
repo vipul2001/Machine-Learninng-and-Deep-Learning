f6b54c806ff37267b1ed5c13f69d6f7a1dc30413
Vipul Bansal <47273021+vipul2001@users.noreply.github.com>
2020-01-15 20:22:00 +0530

    Add files via upload

---
+++ Transfer_learning/code.py
@@ -0,0 +1,282 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import os
+import numpy as np
+import torch
+
+import torchvision
+from torchvision import datasets, models, transforms
+import matplotlib.pyplot as plt
+
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# # Checking for GPU
+
+# In[2]:
+
+
+train_on_gpu = torch.cuda.is_available()
+
+if not train_on_gpu:
+    print('CUDA is not available.  Training on CPU ...')
+else:
+    print('CUDA is available!  Training on GPU ...')
+
+
+# # Loading Dataset
+
+# In[3]:
+
+
+
+# define training and test data directories
+data_dir = 'flower_photos/'
+train_dir = os.path.join(data_dir, 'train/')
+test_dir = os.path.join(data_dir, 'test/')
+
+# classes are folders in each directory with these names
+classes = ['daisy', 'dandelion', 'roses', 'sunflowers', 'tulips']
+
+
+# In[4]:
+
+
+train_dir
+
+
+# In[5]:
+
+
+# VGG-16 Takes 224x224 images as input, so we resize all of them
+data_transform = transforms.Compose([transforms.RandomResizedCrop(224), 
+                                      transforms.ToTensor()])
+
+train_data = datasets.ImageFolder(train_dir, transform=data_transform)
+test_data = datasets.ImageFolder(test_dir, transform=data_transform)
+
+# print out some data stats
+print('Num training images: ', len(train_data))
+print('Num test images: ', len(test_data))
+
+
+# In[6]:
+
+
+batch_size = 20
+num_workers=0
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, 
+                                           num_workers=num_workers, shuffle=True)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, 
+                                          num_workers=num_workers, shuffle=True)
+
+
+# In[7]:
+
+
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy() # convert images to numpy for display
+
+# plot the images in the batch, along with the corresponding labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    plt.imshow(np.transpose(images[idx], (1, 2, 0)))
+    ax.set_title(classes[labels[idx]])
+
+
+# # Loading  pretrained VGG 16 network 
+
+# In[8]:
+
+
+vgg16 = models.vgg16(pretrained=True)
+
+# print out the model structure
+print(vgg16)
+
+
+# In[9]:
+
+
+print(vgg16.classifier[6].in_features) 
+print(vgg16.classifier[6].out_features)
+
+
+# In[10]:
+
+
+for param in vgg16.features.parameters():
+    param.requires_grad = False
+
+
+# In[11]:
+
+
+import torch.nn as nn
+
+n_inputs = vgg16.classifier[6].in_features
+
+# add last linear layer (n_inputs -> 5 flower classes)
+# new layers automatically have requires_grad = True
+last_layer = nn.Linear(n_inputs, len(classes))
+
+vgg16.classifier[6] = last_layer
+
+# if GPU is available, move the model to GPU
+if train_on_gpu:
+    vgg16.cuda()
+
+# check to see that your last layer produces the expected number of outputs
+print(vgg16.classifier[6].out_features)
+print(vgg16)
+
+
+# # Training the changed network results
+
+# In[12]:
+
+
+import torch.optim as optim
+
+# specify loss function (categorical cross-entropy)
+criterion = nn.CrossEntropyLoss()
+
+# specify optimizer (stochastic gradient descent) and learning rate = 0.001
+optimizer = optim.SGD(vgg16.classifier.parameters(), lr=0.001)
+
+
+# In[13]:
+
+
+n_epochs = 2
+a=[]
+for epoch in range(1, n_epochs+1):
+
+    # keep track of training and validation loss
+    train_loss = 0.0
+    
+    ###################
+    # train the model #
+    ###################
+    # model by default is set to train
+    for batch_i, (data, target) in enumerate(train_loader):
+        # move tensors to GPU if CUDA is available
+        if train_on_gpu:
+            data, target = data.cuda(), target.cuda()
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        output = vgg16(data)
+        # calculate the batch loss
+        loss = criterion(output, target)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update training loss 
+        train_loss += loss.item()
+        a.append(loss)
+        if batch_i % 20 == 19:    # print training loss every specified number of mini-batches
+            print('Epoch %d, Batch %d loss: %.16f' %
+                  (epoch, batch_i + 1, train_loss / 20))
+            train_loss = 0.0
+
+
+# # Checking Test Accuracy and making prediction
+
+# In[14]:
+
+
+test_loss = 0.0
+class_correct = list(0. for i in range(5))
+class_total = list(0. for i in range(5))
+
+vgg16.eval() # eval mode
+
+# iterate over test data
+for data, target in test_loader:
+    # move tensors to GPU if CUDA is available
+    if train_on_gpu:
+        data, target = data.cuda(), target.cuda()
+    # forward pass: compute predicted outputs by passing inputs to the model
+    output = vgg16(data)
+    # calculate the batch loss
+    loss = criterion(output, target)
+    # update  test loss 
+    test_loss += loss.item()*data.size(0)
+    # convert output probabilities to predicted class
+    _, pred = torch.max(output, 1)    
+    # compare predictions to true label
+    correct_tensor = pred.eq(target.data.view_as(pred))
+    correct = np.squeeze(correct_tensor.numpy()) if not train_on_gpu else np.squeeze(correct_tensor.cpu().numpy())
+    # calculate test accuracy for each object class
+    for i in range(batch_size):
+        label = target.data[i]
+        class_correct[label] += correct[i].item()
+        class_total[label] += 1
+
+# calculate avg test loss
+test_loss = test_loss/len(test_loader.dataset)
+print('Test Loss: {:.6f}\n'.format(test_loss))
+
+for i in range(5):
+    if class_total[i] > 0:
+        print('Test Accuracy of %5s: %2d%% (%2d/%2d)' % (
+            classes[i], 100 * class_correct[i] / class_total[i],
+            np.sum(class_correct[i]), np.sum(class_total[i])))
+    else:
+        print('Test Accuracy of %5s: N/A (no training examples)' % (classes[i]))
+
+print('\nTest Accuracy (Overall): %2d%% (%2d/%2d)' % (
+    100. * np.sum(class_correct) / np.sum(class_total),
+    np.sum(class_correct), np.sum(class_total)))
+
+
+# In[15]:
+
+
+np.array(a).shape
+
+plt.plot(np.linspace(0,314,314),a)
+
+
+# In[16]:
+
+
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+images.numpy()
+
+# move model inputs to cuda, if GPU available
+if train_on_gpu:
+    images = images.cuda()
+
+# get sample outputs
+output = vgg16(images)
+# convert output probabilities to predicted class
+_, preds_tensor = torch.max(output, 1)
+preds = np.squeeze(preds_tensor.numpy()) if not train_on_gpu else np.squeeze(preds_tensor.cpu().numpy())
+
+# plot the images in the batch, along with predicted and true labels
+fig = plt.figure(figsize=(25, 4))
+for idx in np.arange(20):
+    ax = fig.add_subplot(2, 20/2, idx+1, xticks=[], yticks=[])
+    images=images.cpu()
+    plt.imshow(np.transpose(images[idx], (1, 2, 0)))
+    ax.set_title("{} ({})".format(classes[preds[idx]], classes[labels[idx]]),
+                 color=("green" if preds[idx]==labels[idx].item() else "red"))
+
+
+# In[ ]:
+
+
+
+
