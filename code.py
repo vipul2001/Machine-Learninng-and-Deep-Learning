63eb0055dc91f25a4b03ce1d70888db013f40849
Vipul Bansal <47273021+vipul2001@users.noreply.github.com>
2020-01-18 18:02:09 +0530

    Add files via upload

---
+++ code.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[14]:
+
+
+
+import torch
+import numpy as np
+from torchvision import datasets
+import torchvision.transforms as transforms
+
+# convert data to torch.FloatTensor
+transform = transforms.ToTensor()
+
+# load the training and test datasets
+train_data = datasets.MNIST(root='data', train=True,
+                                   download=True, transform=transform)
+test_data = datasets.MNIST(root='data', train=False,
+                                  download=True, transform=transform)
+
+
+# # Loading Data Set
+
+# In[15]:
+
+
+num_workers = 4
+# how many samples per batch to load
+batch_size = 20
+
+# prepare data loaders
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, num_workers=num_workers)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, num_workers=num_workers)
+
+
+# # Visualizing Data Set
+
+# In[16]:
+
+
+import matplotlib.pyplot as plt
+get_ipython().run_line_magic('matplotlib', 'inline')
+    
+# obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy()
+
+# get one image from the batch
+img = np.squeeze(images[0])
+
+fig = plt.figure(figsize = (5,5)) 
+ax = fig.add_subplot(111)
+ax.imshow(img, cmap='gray')
+
+
+# # Defining Convolutional AutoEncoder
+
+# In[17]:
+
+
+import torch.nn as nn
+import torch.nn.functional as F
+
+# define the NN architecture
+class ConvAutoencoder(nn.Module):
+    def __init__(self):
+        super(ConvAutoencoder, self).__init__()
+        ## encoder layers ##
+        # conv layer (depth from 1 --> 16), 3x3 kernels
+        self.conv1 = nn.Conv2d(1, 16, 3, padding=1)  
+        # conv layer (depth from 16 --> 4), 3x3 kernels
+        self.conv2 = nn.Conv2d(16, 4, 3, padding=1)
+        # pooling layer to reduce x-y dims by two; kernel and stride of 2
+        self.pool = nn.MaxPool2d(2, 2)
+        
+        ## decoder layers ##
+        ## a kernel of 2 and a stride of 2 will increase the spatial dims by 2
+        self.t_conv1 = nn.ConvTranspose2d(4, 16, 2, stride=2)
+        self.t_conv2 = nn.ConvTranspose2d(16, 1, 2, stride=2)
+
+
+    def forward(self, x):
+        ## encode ##
+        # add hidden layers with relu activation function
+        # and maxpooling after
+        x = F.relu(self.conv1(x))
+        x = self.pool(x)
+        # add second hidden layer
+        x = F.relu(self.conv2(x))
+        x = self.pool(x)  # compressed representation
+        
+        ## decode ##
+        # add transpose conv layers, with relu activation function
+        x = F.relu(self.t_conv1(x))
+        # output layer (with sigmoid for scaling from 0 to 1)
+        x = torch.sigmoid(self.t_conv2(x))
+                
+        return x
+
+# initialize the NN
+model = ConvAutoencoder()
+print(model)
+
+
+# # Training Neural Network
+
+# In[18]:
+
+
+criterion = nn.MSELoss()
+
+# specify loss function
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+
+# In[19]:
+
+
+a=[]
+# number of epochs to train the model
+n_epochs = 30
+
+for epoch in range(1, n_epochs+1):
+    # monitor training loss
+    train_loss = 0.0
+    
+    ###################
+    # train the model #
+    ###################
+    for data in train_loader:
+        # _ stands in for labels, here
+        # no need to flatten images
+        images, _ = data
+        # clear the gradients of all optimized variables
+        optimizer.zero_grad()
+        # forward pass: compute predicted outputs by passing inputs to the model
+        outputs = model(images)
+        # calculate the loss
+        loss = criterion(outputs, images)
+        a.append(loss)
+        # backward pass: compute gradient of the loss with respect to model parameters
+        loss.backward()
+        # perform a single optimization step (parameter update)
+        optimizer.step()
+        # update running training loss
+        train_loss += loss.item()*images.size(0)
+            
+    # print avg training statistics 
+    train_loss = train_loss/len(train_loader)
+    print('Epoch: {} \tTraining Loss: {:.6f}'.format(
+        epoch, 
+        train_loss
+        ))
+
+
+# In[30]:
+
+
+np.array(a).shape
+
+plt.plot(np.linspace(0,90000,90000),a)
+plt.show()
+
+
+# # Checking Network Results
+
+# In[20]:
+
+
+
+# obtain one batch of test images
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
+
+# get sample outputs
+output = model(images)
+# prep images for display
+images = images.numpy()
+
+# output is resized into a batch of iages
+output = output.view(batch_size, 1, 28, 28)
+# use detach when it's an output that requires_grad
+output = output.detach().numpy()
+
+# plot the first ten input images and then reconstructed images
+fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(25,4))
+
+# input images on top row, reconstructions on bottom
+for images, row in zip([images, output], axes):
+    for img, ax in zip(images, row):
+        ax.imshow(np.squeeze(img), cmap='gray')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+
+# In[34]:
+
+
+
+# obtain one batch of test images
+#dataiter = iter(test_loader)
+images, labels = dataiter.next()
+
+# get sample outputs
+output = model(images)
+# prep images for display
+images = images.numpy()
+
+# output is resized into a batch of iages
+output = output.view(batch_size, 1, 28, 28)
+# use detach when it's an output that requires_grad
+output = output.detach().numpy()
+
+# plot the first ten input images and then reconstructed images
+fig, axes = plt.subplots(nrows=2, ncols=10, sharex=True, sharey=True, figsize=(25,4))
+
+# input images on top row, reconstructions on bottom
+for images, row in zip([images, output], axes):
+    for img, ax in zip(images, row):
+        ax.imshow(np.squeeze(img), cmap='gray')
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+
+
+# In[ ]:
+
+
+
+
